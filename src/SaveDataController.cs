783f6f2f584ede87676f3b8de9e39354e1567126
Bot Randomness <botrandomness@studio.com>
2023-01-05 15:33:52 -0500

    Added user guided set-up 🖥️

--- src/SaveDataController.cs
+++ src/SaveDataController.cs
@@ -11,24 +11,53 @@ namespace SaveDataController
         {   
             string appLocation = AppDomain.CurrentDomain.BaseDirectory;
             string[] wholeFile = File.ReadAllLines(Path.Combine(appLocation, "res", "SaveData.txt"));
+            string wantPath = "default";
 
             if (wholeFile[3] == "3****" && wholeFile[1] == "1****") 
             {
-                string pathVar = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.User);
-                string newPathVar;
+                while (wantPath != "y" && wantPath != "n") 
+                {
+                    Console.WriteLine("Y: Do you want to add HelloWorld command on to the PATH variable for global use? (Recommended)");
+                    Console.WriteLine("N: Do you only want to use it in the current directory?");
+                    wantPath = Console.ReadLine();
+                    wantPath= wantPath.ToLower();
 
-                wholeFile[1] = pathVar;
-                newPathVar = pathVar + appLocation + ";";
-                wholeFile[3] = newPathVar;
-                File.WriteAllLines(Path.Combine(appLocation, "res", "SaveData.txt"), wholeFile);
-                Environment.SetEnvironmentVariable("PATH", newPathVar, EnvironmentVariableTarget.User);
+                    if (wantPath == "y") 
+                    {
+                        string pathVar = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.User);
+                        string newPathVar;
 
-                FunText.BotImageHead();
+                        wholeFile[1] = pathVar;
+                        newPathVar = pathVar + appLocation + ";";
+                        wholeFile[3] = newPathVar;
+                        File.WriteAllLines(Path.Combine(appLocation, "res", "SaveData.txt"), wholeFile);
+                        Environment.SetEnvironmentVariable("PATH", newPathVar, EnvironmentVariableTarget.User);
 
-                Console.WriteLine("Added to local PATH variable! :)\n");
-                Console.WriteLine("PRESS ANY KEY TO CONTINUE");
-                
-                Console.ReadKey();
+                        FunText.BotImageHead();
+
+                        Console.WriteLine("Added to local PATH variable! :)\n");
+                        Console.WriteLine("SET-UP COMPLETED!");
+                        Console.WriteLine("PRESS ANY KEY TO CONTINUE\n");
+                        
+                        Console.ReadKey();
+                    }
+                    else if (wantPath == "n")
+                    {
+                        wholeFile[1] = "Did not want PATH";
+                        wholeFile[3] = "Did not want PATH";
+                        File.WriteAllLines(Path.Combine(appLocation, "res", "SaveData.txt"), wholeFile);
+
+                        FunText.BotImageHead();
+
+                        Console.WriteLine("Did not add app to local PATH variable");
+                        Console.WriteLine("Only for current directory use");
+                        Console.WriteLine("SET-UP COMPLETED! :)");
+                        Console.WriteLine("PRESS ANY KEY TO CONTINUE\n");
+
+                        Console.ReadKey();
+                    }
+                }
+             
             }
         } 
     }
